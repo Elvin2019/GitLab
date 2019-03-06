7f058322bba270b822b9b01bf4504df12ed31519
Elvin Ahmadov <34287768+Elvin2019@users.noreply.github.com>
2019-03-06 13:15:32 +0400

    Create fwdsdout.c

---
+++ Assignment1/3. fwdstdout/fwdsdout.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    FILE *fileptr;
+   
+    char filechar[40], chr,data[100];
+ 
+    
+    fileptr = fopen("myfile.txt", "a+");
+   //extract character from file and store in chr
+    chr = getc(fileptr);
+    while (chr != EOF)
+    {
+        //Count whenever new line is encountered
+        //take next character from file.
+        strcat(data, &chr);
+        chr = getc(fileptr);
+    }
+    fclose(fileptr); //close file.
+	printf("Enter file name : ");
+    scanf("%s", filechar);
+	fileptr = fopen(filechar,"w");
+	fputs(data,fileptr);
+    return 0;
+}
