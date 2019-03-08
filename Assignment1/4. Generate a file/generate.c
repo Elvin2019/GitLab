147c3a6b5eb8cd6a0ed35b2dce2795a30035b63a
Elvin Ahmadov <34287768+Elvin2019@users.noreply.github.com>
2019-03-08 23:24:14 +0400

    problem 4: argument order corrected, generate

--- Assignment1/4. Generate a file/generate.c
+++ Assignment1/4. Generate a file/generate.c
@@ -10,11 +10,12 @@ int main(int argc ,const char *argv[])
     char filechar[40];
 
     strcpy(filechar,argv[1]);
-	x = strtol(argv[1],NULL,10);
+	x = strtol(argv[2],NULL,10);
     
     fileptr = fopen(filechar, "a+");
+	
     //fseek(fileptr,x,SEEK_SET);
-    ftruncate(fileno(fileptr),x);
+    ftruncate(fileno(fileptr),1024 * x);
 	fclose(fileptr);
     return 0;
 }
