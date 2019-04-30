9271da810f9423fa6655d6cb67310a4c1cba687d
Elvin Ahmadov <34287768+Elvin2019@users.noreply.github.com>
2019-04-30 18:38:58 +0400

    some last edits

--- Assignment3/1. Double_Quit/Double_Quit_main.c
+++ Assignment3/1. Double_Quit/Double_Quit_main.c
@@ -3,84 +3,72 @@
 #include <unistd.h>   
 #include <string.h>   
 #include <signal.h>
+
 static struct sigaction sa;
-static struct sigaction old_sa;
 static int sig_chk=0;//alarm done
 static int ss_tm=0;//alarm done
+
 void handler(int );
 void my_sleep(int );
 void alarm_handler(int );
-void alarm_handler(int signal) {
-    if (signal != SIGALRM) {
-        //fprintf(stderr, "Caught wrong signal: %d\n", signal);
-    }
+
+void alarm_handler(int signal)
+{
     sig_chk=1;
     sigset_t pending;
     if (sigismember(&pending, SIGQUIT)) {
        //printf("Caught Second Time, So exit\n"); 
         ss_tm=1;
-         }
-
-    //printf("Got sigalrm, do_sleep() will end\n");
+    }
 }
+
 void handler(int sgn)
 {
-const char *signal_name;
-//printf("SIGQUIT recorded\n");
-static int cnt=0;
-cnt++;
-if((cnt==2) && (sig_chk != 1))//means before 5 sec another signal come
-{
-printf("Got another before 5 Sec\n");
-cnt=0;
-exit(0);
+	static int cnt=0;
+	cnt++;
+	if((cnt==2) && (sig_chk != 1)){ //means before 5 sec another signal come 
+		printf(" Double quit signal before 5 Sec\n");
+		cnt=0;
+		exit(0);
+	}
+	else if((cnt==2) && (sig_chk == 1)){ //means second signal comes after 5 sec means reinit
+		cnt=1;
+		sig_chk = 0;
+	}
+	my_sleep(5);
 }
-else if ((cnt==1))//means first time
-{
-//printf("Now Wait For 5 Seconds to get other signal\n");
-}
-else if((cnt==2) && (sig_chk == 1) )//means second signal comes after 5 sec means reinit
-{
-cnt=1;
-sig_chk = 0;
-}
-my_sleep(5);
-}
+
 void my_sleep(int sec)
 {
-struct sigaction sa;
-sigset_t mask;
-sa.sa_flags = SA_RESTART;
-sa.sa_handler = &alarm_handler; // Intercept and ignore SIGALRM
-//sa.sa_flags = SA_RESETHAND; // Remove the handler after first signal
-sigfillset(&sa.sa_mask);
-sigaction(SIGALRM, &sa, NULL);
-// Get the current signal mask
-sigprocmask(0, NULL, &mask);
+	struct sigaction sa;
+	sigset_t mask;
+	sa.sa_flags = SA_RESTART;
+	sa.sa_handler = &alarm_handler; // Intercept and ignore SIGALRM
+	//sa.sa_flags = SA_RESETHAND; // Remove the handler after first signal
+	sigfillset(&sa.sa_mask);
+	sigaction(SIGALRM, &sa, NULL);
+	// Get the current signal mask
+	sigprocmask(0, NULL, &mask);
 
-// Unblock SIGALRM
-sigdelset(&mask, SIGALRM);
-sigdelset(&mask, SIGQUIT);
 
 // Wait with this mask
-alarm(sec);
+	alarm(sec);
 //sigsuspend(&mask);
 }
 int main() 
 {  
-char str[100];
-int len;
-sa.sa_handler = &handler;
-sa.sa_flags = SA_RESTART;
-sigfillset(&sa.sa_mask);
-if (sigaction(SIGQUIT, &sa, NULL) == -1) {
+	char str[100];
+	int len;
+	sa.sa_handler = &handler;
+	sa.sa_flags = SA_RESTART;
+	sigfillset(&sa.sa_mask);
+	if (sigaction(SIGQUIT, &sa, NULL) == -1){
         perror("Error: cannot handle SIGQUIT"); // Should not happen
-}
-   while(1)
-   {
+	}
+   while(1){
       write(1,"Enter Text :\n",14);
       len=read(0,str,100);
-      write(1,"Given Text :\n",14);
+	  write(1,"Given Text :\n",14);
       write(1,str,len);
       memset(str,EOF,100);
    } 
